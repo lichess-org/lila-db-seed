b69bfd0597d2aed05e7cb3815f37df395a2dbee0
tontsa28 <miika@tontsa.fi>
2024-08-26 13:47:35 +0300

    fix: improve the veracity of the generated data

--- spamdb/modules/storm.py
+++ spamdb/modules/storm.py
@@ -1,4 +1,5 @@
 from random import randint
+from datetime import datetime
 from modules.env import env
 import modules.util as util
 
@@ -22,11 +23,11 @@ def update_storm_colls() -> None:
 
 class StormRound:
     def __init__(self, uid: str):
-        self._id = f'{uid}:5347'
-        self.score = randint(0, 100)
+        self._id = f'{uid}:{(datetime.today() - datetime(2010, 1, 1)).days}' # Get the correct day integer that represents the current date
+        self.score = randint(1, 100)
         self.moves = self.score + randint(0, 50)
         self.errors = randint(0, self.score)
         self.combo = randint(0, self.moves)
-        self.time = randint(0, 255)
+        self.time = randint(100, 255)
         self.highest = randint(1000, 3000)
-        self.runs = randint(0, 25)
+        self.runs = randint(1, 10)
