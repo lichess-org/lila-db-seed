3132fa2414583aa88997f638ab95f342464aeb48
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2024-12-14 21:45:27 -0500

    add special users who can manage cms pages and add events

--- spamdb/modules/user.py
+++ spamdb/modules/user.py
@@ -288,6 +288,8 @@ def _create_special_users():
         users.append(User("shusher", [], ["ROLE_SHUSHER"], False))
         users.append(User("hunter", [], ["ROLE_CHEAT_HUNTER"], False))
         users.append(User("puzzler", [], ["ROLE_PUZZLE_CURATOR"], False))
+        users.append(User("editor", [], ["ROLE_PAGES"], False))
+        users.append(User("events", [], ["ROLE_MANAGE_EVENT"], False))
     users.append(User("broadcaster", [], [], False))
     users.append(User("api", [], ["ROLE_API_HOG"], False))
     users.append(User("troll", ["troll"], [], False))
